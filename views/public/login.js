a2937963f24dc20b9091b3e4aaa65765f6227940
Mohit <20bma048@nith.ac.in>
2023-09-09 15:42:18 +0530

    updated

--- views/public/login.js
+++ views/public/login.js
@@ -1,18 +1,15 @@
-// JavaScript for handling login form submission
 document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.getElementById("login-form");
   
     loginForm.addEventListener("submit", function (e) {
       e.preventDefault();
   
-      // Collect form data
       const formData = new FormData(loginForm);
       const formDataObject = {};
       formData.forEach((value, key) => {
         formDataObject[key] = value;
       });
   
-      // Send the form data to the API endpoint for login
       fetch("http://localhost:3000/api/v1/auth/login", {
         method: "POST",
         headers: {
@@ -23,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((response) => {
           if (response.ok) {
             alert("Signup successful!");
-            window.location.href = "http://127.0.0.1:5500/views/dashboard.html"; // Change this to the actual home page URL
+            window.location.href = "http://127.0.0.1:5500/views/index.html"; // Change this to the actual home page URL
           } else {
             // Handle login failure (e.g., display an error message)
             alert("Login failed. Please check your email and password.");
